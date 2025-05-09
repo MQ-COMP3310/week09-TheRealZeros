f6777803ecc756da7948a5db44eea0cdba65fd09
Gabriel D'Costa <65882215+Gabriel-dcosta@users.noreply.github.com>
2025-05-09 10:21:05 +1000

    SQL Injection fix & host IP changed

--- project/auth.py
+++ project/auth.py
@@ -38,8 +38,11 @@ def signup_post():
     email = request.form.get('email')
     name = request.form.get('name')
     password = request.form.get('password')
+    # no sanitising
 
-    user = db.session.execute(text('select * from user where email = "' + email +'"')).all()
+    signup_query = text('select * from user where email = :email')
+
+    user = db.session.execute(signup_query).all()
     if len(user) > 0: # if a user is found, we want to redirect back to signup page so user can try again
         flash('Email address already exists')  # 'flash' function stores a message accessible in the template code.
         app.logger.debug("User email already exists")
--- project/models.py
+++ project/models.py
@@ -2,7 +2,8 @@ from flask_login import UserMixin
 from . import db
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy 
+    # Using Integer for id is incorrect. Its inuberable. Set to string and uisng UUID, Snowflake or other libraries to generate unique id.
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
-    name = db.Column(db.String(1000))
+    name = db.Column(db.String(1000)) 
